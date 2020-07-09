eaab6597888131f2a1c1c67605dd2cb656db755e
crajhansa <chaitanya.rajhansa@gmail.com>
2020-07-08 19:50:05 -0700

    added search and filter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import Header from './components/ui/Header.js'
+import Search from './components/ui/Search.js'
 import CharacterGrid from './components/characters/CharacterGrid.js'
 import './App.css';
 import axios from 'axios';
@@ -7,18 +8,19 @@ import axios from 'axios';
 const App = () => {
   const [items, setItems] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const [query, setQuery] = useState('')
 
   useEffect(() => {
     const fetchItems = async () => {
-      const result = await axios('https://www.breakingbadapi.com/api/characters')
-      console.log(result.data)
+      const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`)
       setItems(result.data)
       setIsLoading(false);
     }
     fetchItems()
-  }, [])
+  }, [query])
   return <div className="container">
     <Header />
+    <Search getQuery={(q)=>setQuery(q)} />
     <CharacterGrid isLoading={isLoading} items={items} />
   </div>
 }
